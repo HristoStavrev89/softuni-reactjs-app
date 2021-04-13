b96f473d12a323688d281adbebce3908baa13f90
HristoStavrev89 <hristo.stavrev@yahoo.com>
2021-04-13 13:45:02 +0200

    details page test

--- src/components/OfferDetails/OfferDetails.js
+++ src/components/OfferDetails/OfferDetails.js
@@ -7,31 +7,66 @@ import './OfferDetails.css'
 
 export default function OfferDetails(props) {
 
-    const [ details, setDetails ] = useState();
+    const [details, setDetails] = useState();
     const offerId = props.match.params.offerId;
-    
-    
+
+
     useEffect(() => {
         db.collection("cars")
-        .doc(offerId)
-        .get()
-        .then(response => {
-            const currentOffer = response.data();
-            setDetails(currentOffer)
-        })
+            .doc(offerId)
+            .get()
+            .then(response => {
+                const currentOffer = response.data();
+                setDetails(currentOffer)
+            })
     }, [])
 
 
-        console.log(details);
-    
+
 
     return (
         <>
-            <div>
-                <h1 className="test-heading">This is Offer details page for {details && details.brand}</h1>
+            { details && (
+                <div className="details-container">
+                    <h1 className="details-title">Details</h1>
+                    <div className="details-wrapper" >
+                        {/* This is just a test */}
+                        <form className="edit-form" action="submit">
+                            <div class="field-input">
+                                <h4>Title</h4>
+                                <input type="text" defaultValue={details.title} class="form-control" placeholder="You need to add title" required />
+                            </div>
 
-            </div>
+                            <div class="field-input">
+                                <h4>Brand</h4>
+                                <input type="text" defaultValue={details.brand} class="form-control" placeholder="You need to add brand" required />
+                            </div>
 
+                            <div class="field-input">
+                                <h4>Model</h4>
+                                <input type="text" defaultValue={details.model} class="form-control" placeholder="You need to add model" required />
+                            </div>
+
+                            <div class="field-input">
+                                <h4>Year</h4>
+                                <input type="number" defaultValue={details.year} class="form-control" placeholder="You need to add year" required />
+                            </div>
+
+                            <div class="field-input">
+                                <h4>Price</h4>
+                                <input type="number" defaultValue={details.price} class="form-control" placeholder="You need to add year" required />
+                            </div>
+
+                            <div class="field-input">
+                                <h4>Description</h4>
+                                <textarea class="form-control" defaultValue={details.description} required ></textarea>
+                            </div>
+                            <button className="submitBtn" type="submit">Edit</button>
+                        </form>
+                    </div>
+
+                </div>
+            )}
         </>
     )
 }
