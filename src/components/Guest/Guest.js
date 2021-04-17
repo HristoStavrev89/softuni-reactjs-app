7eb95f5b2eddf76da0b3383912700e34efb2af8c
HristoStavrev89 <hristo.stavrev@yahoo.com>
2021-04-17 10:22:11 +0200

    new paragraph in guest page

--- src/components/Guest/Guest.js
+++ src/components/Guest/Guest.js
@@ -8,6 +8,7 @@ export default function Guest() {
             <Container className="d-flex align-items-center justify-content-center imgContainer-guest">
                 <div className="w-100 wrapper" >
                     <h1 className="greet" >Welcome to <span>Auto Nation</span></h1>
+                    <p>Find your dream car now.</p>
                     
                 </div>
             </Container>
